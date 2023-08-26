baaaeb9847288d422b50d96d17f352a9d0fbe7bb
Amit Bilapatte <agbilapatte78@gmail.com>
2023-08-26 16:05:23 +0530

    Add axios, use async thunks

--- app/store.js
+++ app/store.js
@@ -3,6 +3,7 @@ const reduxLogger = require("redux-logger");
 const cakeReducer = require("../features/cake/cakeSlice");
 const icecreamReducer = require("../features/icecream/icecreamSlice");
 const { getDefaultMiddleware } = require("@reduxjs/toolkit");
+const userReducer = require("../features/user/userSlice");
 
 const logger = reduxLogger.createLogger();
 
@@ -11,6 +12,7 @@ const store = configureStore({
   reducer: {
     cake: cakeReducer,
     icecream: icecreamReducer,
+    user: userReducer,
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
---
+++ features/user/userSlice.js
@@ -0,0 +1,34 @@
+const createSlice = require("@reduxjs/toolkit").createSlice;
+
+const createAsyncThunk = require("@reduxjs/toolkit").createAsyncThunk;
+const axios = require("axios");
+
+const initialState = {
+  loading: false,
+  users: [],
+  error: "",
+};
+
+const fetchUsers = createAsyncThunk("user/fetchUSers", () => {
+  // createAsyncThunk generates pending, fulfilled and rejected action types
+  return axios.get("https://jsonplaceholder.typicode.com/users").then((res) => res.data.map((user) => user.name));
+});
+
+const userSlice = createSlice({
+  name: "user",
+  initialState,
+  extraReducers: (builder) => {
+    builder.addCase(fetchUsers.pending, (state) => {
+      state.loading = true;
+    });
+    builder.addCase(fetchUsers.fulfilled, (state, action) => {
+      (state.loading = false), (state.users = action.payload), (state.error = "");
+    });
+    builder.addCase(fetchUsers.rejected, (state, action) => {
+      (state.loading = false), (state.users = []), (state.error = action.error.message);
+    });
+  },
+});
+
+module.export = userSlice.reducer;
+module.exports.fetchUsers = fetchUsers;
