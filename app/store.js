bd0a3ec6e43648530e523377b64e096f3483747f
Amit Bilapatte <agbilapatte78@gmail.com>
2023-08-26 12:42:38 +0530

    Add comments for explaining code working

--- app/store.js
+++ app/store.js
@@ -15,4 +15,21 @@ const store = configureStore({
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
 
+//example logs due to middleware after running index.js:
+
+// prev state { cake: { numOfCakes: 8 }, icecream: { numOfIcecream: 10 } }
+// action     { type: 'cake/ordered', payload: undefined }
+// next state { cake: { numOfCakes: 7 }, icecream: { numOfIcecream: 10 } }
+// action cake/restocked @ 12:38:31.374
+// prev state { cake: { numOfCakes: 7 }, icecream: { numOfIcecream: 10 } }
+// action     { type: 'cake/restocked', payload: 5 }
+// next state { cake: { numOfCakes: 12 }, icecream: { numOfIcecream: 10 } }
+// action icecream/ordered @ 12:38:31.374
+// prev state { cake: { numOfCakes: 12 }, icecream: { numOfIcecream: 10 } }
+// action     { type: 'icecream/ordered', payload: undefined }
+// next state { cake: { numOfCakes: 12 }, icecream: { numOfIcecream: 9 } }
+// action icecream/ordered @ 12:38:31.375
+
+// as we can see it takes actions types automatically from the reducer names we defined i.e. type: 'cake/ordered',type: 'cake/restocked',type: 'icecream/ordered'
+
 module.exports = store;
