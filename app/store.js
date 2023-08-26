391bfd71e03104478c4ff693b0c5c373989d331b
Amit Bilapatte <agbilapatte78@gmail.com>
2023-08-26 12:36:03 +0530

    1) install redux-logger 2) remove logs and use redux-logger as middleware

--- app/store.js
+++ app/store.js
@@ -1,6 +1,10 @@
 const configureStore = require("@reduxjs/toolkit").configureStore;
+const reduxLogger = require("redux-logger");
 const cakeReducer = require("../features/cake/cakeSlice");
 const icecreamReducer = require("../features/icecream/icecreamSlice");
+const { getDefaultMiddleware } = require("@reduxjs/toolkit");
+
+const logger = reduxLogger.createLogger();
 
 const store = configureStore({
   //here configureStore handles work of combineReducer internally
@@ -8,6 +12,7 @@ const store = configureStore({
     cake: cakeReducer,
     icecream: icecreamReducer,
   },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
 
 module.exports = store;
